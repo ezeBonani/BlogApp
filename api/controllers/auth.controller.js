735d379c8cb2455464697a4e6bad45b5807329bf
Ezequiel Luis Bonani <eze.bonani@gmail.com>
2024-03-25 22:10:08 -0300

    create signin auth

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -2,6 +2,7 @@ import User from "../models/user.model.js";
 /* instalar el paquete bcryptjs para hashear password en base de datos */
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 /* funcion asincrona, requiere procesamiento */
 export const signup = async (req, res, next) => {
@@ -31,6 +32,35 @@ export const signup = async (req, res, next) => {
     res.json("Signup successful");
   } catch (error) {
     /* si repito usuario o email salta error*/
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password || email === "" || password === "") {
+    next(errorHandler(400, "All fields are required"));
+  }
+
+  try {
+    const validUser = await User.findOne({ email }); //metodo de busqueda en mongoDB
+    if (!validUser) {
+      return next(errorHandler(404, "User not found"));
+    }
+    const validPassword = bcryptjs.compareSync(password, validUser.password); //comparo password con el hash password
+    if (!validPassword) {
+      return next(errorHandler(400, "Invalid password, try again"));
+    }
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET); //el campo _id lo genera mongoDB
+
+    const { password: pass, ...rest } = validUser._doc; //saco el password
+
+    res
+      .status(200)
+      .cookie("access_token", token, { httpOnly: true })
+      .json(rest);
+  } catch (error) {
     next(error);
   }
 };
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,8 +1,12 @@
 import express from "express";
-import { signup } from "../controllers/auth.controller.js"; /* no olvidar siempre poner la extension .js */
+import {
+  signin,
+  signup,
+} from "../controllers/auth.controller.js"; /* no olvidar siempre poner la extension .js */
 
 const router = express.Router();
 
 router.post("/signup", signup);
+router.post("/signin", signin);
 
 export default router;
