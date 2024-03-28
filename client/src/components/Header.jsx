8de81fd4989ca6a4bdd480aee96f44381dc8a18a
Ezequiel Luis Bonani <eze.bonani@gmail.com>
2024-03-27 22:46:50 -0300

    header with user data

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,12 @@
-import { Button, Navbar, TextInput } from "flowbite-react";
+import { Avatar, Button, Dropdown, Navbar, TextInput } from "flowbite-react";
 import { Link, useLocation } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
 import { FaMoon } from "react-icons/fa";
+import { useSelector } from "react-redux";
 
 export default function Header() {
   const path = useLocation().pathname;
+  const { currentUser } = useSelector((state) => state.user);
 
   return (
     <Navbar className="border-b-2">
@@ -35,11 +37,33 @@ export default function Header() {
         <Button className="w-12 h-10 hidden sm:inline" color="lime" pill>
           <FaMoon /> {/* boton de tema claro/oscuro */}
         </Button>
-        <Link to="/sign-in">
-          <Button gradientDuoTone="tealToLime" outline>
-            Sign In
-          </Button>
-        </Link>
+        {currentUser ? (
+          <Dropdown
+            arrowIcon={false}
+            inline
+            label={
+              <Avatar alt="user" img={currentUser.profilePicture} rounded />
+            }
+          >
+            <Dropdown.Header>
+              <span className="block text-sm">@{currentUser.username}</span>
+              <span className="block text-me font-medium truncate">
+                {currentUser.email}
+              </span>
+            </Dropdown.Header>
+            <Link to={"/dashboard?tab=profile"}>
+              <Dropdown.Item>Profile</Dropdown.Item>
+              <Dropdown.Divider />
+              <Dropdown.Item>Sign Out</Dropdown.Item>
+            </Link>
+          </Dropdown>
+        ) : (
+          <Link to="/sign-in">
+            <Button gradientDuoTone="tealToLime" outline>
+              Sign In
+            </Button>
+          </Link>
+        )}
         <Navbar.Toggle /> {/* hamburguersita */}
       </div>
 
